e9dfe7c15a59b83e494350fe3a629fce310ad1d1
Tristan <tristan.philippe0@outlook.com>
2025-07-29 14:51:56 +0800

    First commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import logo from './logo.svg';
+import Web3 from "web3";
 import './App.css';
 
 function App() {
@@ -7,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello world!
         </p>
         <a
           className="App-link"
